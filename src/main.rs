b578ba1518b25a01d7b4a9bec2d575abb33a7cd0
Pirmin Kalberer <pka@sourcepole.ch>
2019-11-27 23:28:47 +0100

    Actix web hello world

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+fn main() {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")
+    .expect("Can not bind to port 8000")
+    .run()
+    .unwrap();
+}
